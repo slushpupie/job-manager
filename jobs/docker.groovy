1b64ea93e08e6ba262233282303050e27b9529cb
Jay Kline <jay@slushpupie.com>
2015-08-27 22:30:21 -0500

    Use a loop

--- jobs/docker.groovy
+++ jobs/docker.groovy
@@ -1,37 +1,25 @@
 
 folder("docker")
 
-job("docker/jenkins-slave") {
-  scm {
-    github("slushpupie/docker-jenkins-slave","ssh")
-  }
+def images = ["jenkins-slave", "bind9", "docker-proxy"]
 
-  triggers {
-    githubPush()
-  }
+images.each {
 
-  steps {
-    shell("""
-      docker build -t slushpupie/jenkins-slave -t localhost:6000/slushpupie/jenkins-slave docker-jenkins-slave
-      docker push localhost:6000/slushpupie/jenkins-slave
-    """)
-  }
+  job("docker/jenkins-slave") {
+    scm {
+      github("slushpupie/docker-${it}","ssh")
+    }
 
-}
+    triggers {
+      scm("H * * * *")
+    }
 
-job("docker/bind9") {
-  scm { 
-    github("slushpupie/docker-bind9","ssh")
-  }
-
-  triggers {
-    scm("H * * * *")
-  }
-
-  steps {
-    shell("""
-      docker build -t slushpupie/bind9 -t localhost:6000/slushpupie/bind9 docker-bind9
-      docker push localhost:6000/slushpupie/bind9
-    """)
+    steps {
+      shell("""
+        docker build -t slushpupie/${it} -t localhost:6000/slushpupie/${it} ${it}
+        docker push localhost:6000/slushpupie/${it}
+      """)
+    }
   }
 }
+
