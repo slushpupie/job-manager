d6e808edbf02f40353ecda575e9d7d6e118035d6
Jay Kline <jay@slushpupie.com>
2015-08-27 22:23:36 -0500

    Finish bind9 job

--- jobs/docker.groovy
+++ jobs/docker.groovy
@@ -23,3 +23,15 @@ job("docker/bind9") {
   scm { 
     github("slushpupie/docker-bind9","ssh")
   }
+
+  triggers {
+    scm("H * * * *")
+  }
+
+  steps {
+    shell("""
+      docker build -t slushpupie/bind9 -t localhost:6000/slushpupie/bind9 docker-bind9
+      docker push localhost:6000/slushpupie/bind9
+    """)
+  }
+}
