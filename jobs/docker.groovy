bebb2be7f3297a668ba96e9af753d346b40bfbc1
Jay Kline <jay@slushpupie.com>
2015-09-13 21:08:59 -0500

    update the docker update

--- jobs/docker.groovy
+++ jobs/docker.groovy
@@ -41,7 +41,7 @@ job("docker/update") {
     }
 
     steps {
-        ["jenkins", "ubuntu", "nginx", "elasticsearch", "logstash", "kibana", "larsks/crashplan", "evarga/jenkins-slave", "mhimmer/dropbox", "sameersbn/squid", "bind9"].each { image ->
+        ["jenkins", "ubuntu", "nginx", "elasticsearch", "logstash", "kibana", "larsks/crashplan", "evarga/jenkins-slave", "mhimmer/dropbox", "sameersbn/squid"].each { image ->
           shell("""
             docker pull ${image}
           """)
